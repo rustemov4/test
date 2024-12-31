280e855445911897c532f069d3f47bbd7db8f541
rustemov4 <azamat.rustemov.02@mail.ru>
2025-01-01 02:33:53 +0500

    bubble sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,4 +2,25 @@ package org.example;public class Main {
     public static void main(String[] args) {
         System.out.println("after gitignore!");
     }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            // Perform bubble sort
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no elements were swapped, break early
+            if (!swapped) break;
+        }
+    }
 }
