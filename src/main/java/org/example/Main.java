727ff7adcaaa03dfe61e5bee1001d120cc353509
rustemov4 <azamat.rustemov.02@mail.ru>
2025-01-01 02:29:11 +0500

    Revert "some operation"
    This reverts commit d7771e4e34d882f4dc29b82368e4ae020ab2fa5d.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;public class Main {
     public static void main(String[] args) {
-        System.out.println("some operation!");
+        System.out.println("after gitignore!");
     }
 }
