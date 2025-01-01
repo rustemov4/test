b1e919b6b32c81d4f55b93e23f50ca9609247da8
rustemov4 <azamat.rustemov.02@mail.ru>
2025-01-01 13:42:15 +0500

    simple sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("after gitignore!");
+        int[] array = {5, 2, 9, 1, 5, 6};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+
+        Arrays.sort(array);
+
+        System.out.println("Sorted Array (Built-in Sort): " + Arrays.toString(array));
     }
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
 
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            // Perform bubble sort
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap elements
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            // If no elements were swapped, break early
-            if (!swapped) break;
-        }
-    }
 }
